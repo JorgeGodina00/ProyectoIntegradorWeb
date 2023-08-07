c40b37925c4b00e11bf9e6c75d579aac0a5e2b65
JorgeGodina00 <92189696+JorgeGodina00@users.noreply.github.com>
2023-08-07 15:30:19 -0600

    Push 07/08/2023
    A tan solo 14 horas de la presentacion del Integrador, se hacen los cambios

--- app.py
+++ app.py
@@ -2,71 +2,33 @@ from flask import Flask, render_template, request, redirect, url_for, flash #imp
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+#BD
 app.config['MYSQL_HOST']="localhost"
 app.config['MYSQL_USER']="root"
 app.config['MYSQL_PASSWWORD']=""
-app.config['MYSQL_DB']="bd_integrador"
+app.config['MYSQL_DB']="proyecto"
 
 app.secret_key='mysecretkey'
 
 mysql = MySQL(app)
 
+#Rutas
 
 @app.route('/')
 def index():
     return render_template ('index.html')
 
-@app.route('/inicia_sesion')
-def login():
-    return render_template('login.html')
-   
-@app.route("/historia")
-def historia():
-    return render_template ('historia.html')
+@app.route('/servicio')
+def servicio():
+    return render_template('servicio.html')
 
-@app.route("/soporte")
-def mantenimiento():
-    return render_template('soporte.html')
+@app.route('/acerca')
+def acerca():
+    return render_template('acerca.html')
 
-@app.route("/servicios")
-def servicios():
-    return render_template("servicios.html")
-
-@app.route("/ajustes")
-def ajustes():
-    return render_template("ajustes.html")
-
-@app.route('/guardarservicio', methods=['POST'])
-def guardar():
-    if request.method == 'POST':
-        Vcliente= request.form['txtid_cliente']
-        Vserv= request.form['txtcve_serv']
-        Vcorreo= request.form['txtemail']
-        Vtel= request.form['txttel']
-        Vfecha= request.form['txtfecha']
-        Vcosto= request.form['txtcosto']
-        
-        
-        CS = mysql.connection.cursor() #Variable de tipo cursor que contiene las herramientas paara realizar los querys
-        CS.execute("INSERT INTO servicio_cliente(id_cliente, cve_serv, costo, numero, fecha, correo) VALUES (%s, %s, %s, %s, %s, %s)", (Vcliente, Vserv, Vcosto, Vtel, Vfecha, Vcorreo))
-        mysql.connection.commit()
-       
-    return redirect(url_for('index'))
-
-@app.route("/buscar")
-def consultar():
-    return render_template("buscar.html")
-
-@app.route('/editarservicio/<id>')
-def editar(id):  
-    curEditar= mysql.connection.cursor()
-    curEditar.execute('Select * from  Where id= %s ', (id,))
-    consulId= curEditar.fetchone()
-    return render_template('editarfruta.html',fruta= consulId)
-
-@app.route('/contacto')
-def contacto():
-    return render_template('contacto.html')
-
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 if __name__ == '__main__':
     app.run(debug= True, port= 5900)
