2b1f34f1c8abb1c6bc3b2b4a8f64430711021bd6
RomanFF123 <121040623@upq.edu.mx>
2023-08-07 20:05:38 -0600

    Ruta Info Cliente

--- app.py
+++ app.py
@@ -30,5 +30,26 @@ def acerca():
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
+
+
+
+@app.route('/infocliente', methods=['POST'])
+def newmedico():
+    if request.method == 'POST':
+        Vnombre= request.form['nametxt']
+        Vapellidop= request.form['aptxt']
+        Vapellidom= request.form['amtxt']
+        Vtelefono= request.form['teltxt']
+        Vemail= request.form['emailtxt']
+        Vdesc= request.form['desctxt']   
+        CS = mysql.connection.cursor() 
+        CS.execute("INSERT INTO formulario1 (Nombre, Apellido1, Apellido2, Email, Telefono, Descripcion) VALUES (%s, %s, %s, %s, %s, %s)", (Vnombre, Vapellidop, Vapellidom, Vtelefono, Vemail, Vdesc))
+        mysql.connection.commit()
+    flash('Se ha hecho el registro correctamente')    
+    return redirect(url_for('index'))
+
+
+
+
 if __name__ == '__main__':
     app.run(debug= True, port= 5900)
