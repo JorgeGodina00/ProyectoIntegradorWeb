f987e0a249ff766f266bec2aa831f31dc1239546
JorgeGodina00 <92189696+JorgeGodina00@users.noreply.github.com>
2023-08-08 03:21:48 -0600

    08/08/2023
    Push a las 3:21 am

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ mysql = MySQL(app)
 def index():
     return render_template ('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/servicio')
 def servicio():
     return render_template('servicio.html')
